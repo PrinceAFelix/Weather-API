439d2d1a8e77696e9e8002759fb021a08387b494
Prince Felix <prince@Princes-MacBook-Pro.local>
2022-07-16 12:14:12 -0400

    Finished everything - Just need retouch and responsiveness to allow compatibility in different resolutions

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,17 @@
 'use strict'
 import {Sun, Weather} from './sun-model.js'
-import {getCurrentWeather, getFutureWeather, getCityCoordinates} from './weather-api-service.js'
+import {getCurrentWeather, getFutureWeather, getCityCoordinates, createWeatherIcon} from './weather-api-service.js'
 
 var sunsetDt;
 var sunriseDt;
 var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-var weatherObj;
+let avg = [];
+
+let total = [];
+let counter = 0;
+
+let low = [], high = []
 
 const main = {
     locKey: 'pk.6eff11d2dd5c4fbf4f82d3ed41476434',
@@ -18,9 +23,7 @@ const main = {
         return main.displayWeather(data);
     },
     fetchFutureWeather: async (lat, lon) =>{
-         console.log("fetchFutureWeather")
         const data = await getFutureWeather(lat, lon);
-       
         return await main.displayFutureWeather(data);
     },
     fetchCityCoord: async (city) =>{
@@ -33,27 +36,46 @@ const main = {
         const { icon, description } = data.weather[0];
         const { sunset, sunrise } = data.sys;
         const { speed, deg } = data.wind;
-    
+
         var celciusTemp = Math.round(temp - 273.15);
         var minTemp = Math.round(temp_min - 273.15);
         var maxTemp = Math.round(temp_max - 273.15);
         var feelsTemp = Math.round(feels_like - 273.15);
-    
         sunsetDt = new Date(sunset*1000);
         sunriseDt = new Date(sunrise*1000);
-    
+
+
+        let des = data.weather[0].main.toLowerCase();
+
+        document.querySelector('body').setAttribute('state', icon.slice(-1)+ (des == 'mist' || des == 'haze' ? 'fog' : des))
+        let day = icon.slice(-1) == 'd' ? 'd' : 'n';
+        let elements = document.getElementsByClassName('futureWeather');
+        if(day == 'd'){
+            document.querySelector('body').style.backgroundColor = '#2175B3';
+            for(let i = 0; i < elements.length; i++){
+                elements[i].style.backgroundColor = '#1C68A7';
+            }
+        }else{
+            document.querySelector('body').style.backgroundColor = '#1E1D32';
+            for(let i = 0; i < elements.length; i++){
+                elements[i].style.backgroundColor = '#22243C';
+            }
+        }
+        //Get today's max and min temp
+        low.push(minTemp)
+        high.push(maxTemp)
+
         //Capitalize first letter
         var desc = description.split(" ");
         for (let i = 0; i < desc.length; i++) {
             desc[i] = desc[i][0].toUpperCase() + desc[i].substr(1);
         }
-        
         document.querySelector(".city").innerHTML = name;
-        document.querySelector(".temp").innerHTML = celciusTemp + '°';
+        document.querySelector(".temp").innerHTML = celciusTemp + '&#176;';
         document.querySelector(".description").innerHTML = desc.join(" ");;
-        document.querySelector(".min").innerHTML = "L:" + minTemp + '°';
-        document.querySelector(".max").innerHTML = "H:" + maxTemp + '°';
-        document.querySelector(".feels-like-temp").innerHTML = feelsTemp + '°';
+        document.querySelector(".min").innerHTML = "L:" + minTemp + '&#176;';
+        document.querySelector(".max").innerHTML = "H:" + maxTemp + '&#176;';
+        document.querySelector(".feels-like-temp").innerHTML = feelsTemp + '&#176;';
         document.querySelector(".flt-msg").innerHTML = main.feelsTemperatureMsg(celciusTemp, feelsTemp);
         document.querySelector(".humid").innerHTML = humidity + '%';
         document.querySelector(".dew-point").innerHTML = main.dewPointCalc(celciusTemp, humidity);
@@ -64,35 +86,37 @@ const main = {
         document.getElementById("speedLabel").innerHTML = "km/h";
         document.querySelector(".hPa").innerHTML = pressure.toLocaleString();
         document.getElementById("hpaLabel").innerHTML = "hPa";
-
-        document.getElementById("low-label1").innerHTML = minTemp + '°';
-        document.getElementById("high-label1").innerHTML = maxTemp + '°';
-        
     },
     displayFutureWeather: async function (data) {
-        var tempMinAvg, tempMaxAvg, dayOneAvg, dayTwoAvg, dayThreeAvg, dayFourAvg, dayFiveAvg;
-
         var iconAvg = [];
-        var tempAvg = [];
-
-        var weatherIconSRC = "";
+        let weatherIconSRC = "";
         var time;
-        var main = "";
+        var mainDesc = "";
         var sun = new Sun(sunsetDt.getHours() % 12, sunriseDt.getHours() % 12);
-        
-        
-        for(var i = 0; i <= 39; i++){
-            main = data.list[i].weather[0].main;
-            var dt = new Date(data.list[i].dt * 1000);
-            console.log(dt);
-            time = this.convertTime(dt, true);
-            
-            var wholeNumberTime = time.replace(/\D/g, "");
 
-            
+
+        let hourly =  document.querySelector(".hourly-forecast");
+        if (hourly.querySelector('div')) {
+            hourly.querySelectorAll('div').forEach(div => div.remove())
+        }
+        
+
+        let isDayPassed = false;
+
+
+        //item.main.temp
+        data.list.forEach((item, index) => {
+            var dt = new Date(item.dt * 1000);
+            time = this.convertTime(dt, true);
+            let div = document.createElement('div');
+            div.className = 'future-forecast'
+            var wholeNumberTime = time.replace(/\D/g, "");
+            mainDesc = item.weather[0].main;
+
+
             if(time.includes("PM") &&  wholeNumberTime >= sun.sunset || time.includes("AM") && wholeNumberTime <= sun.sunrise){
                 var tempName = "";
-                switch (main) {
+                switch (mainDesc) {
                     case "Clear":
                         tempName = "Clear-night"
                         break;
@@ -105,57 +129,91 @@ const main = {
                     default:
                         break;
                 }
-                weatherIconSRC = "/icons/weather-icons/" + tempName + ".png";
+                weatherIconSRC = tempName;
             }else{
-                weatherIconSRC = "/icons/weather-icons/" + main + ".png";
+                weatherIconSRC = mainDesc;
             }
 
-            if(time.toString() == "2PM"){
-                iconAvg.push(weatherIconSRC)
-            }
-    
-            //console.log(i + ": " + weatherIconSRC);
+            if(time.includes("2PM")) iconAvg.push(weatherIconSRC)
 
+
+            if(index <= 20){
+                div.innerHTML = `<h3>${time}</h3>`
             
-     
-            document.getElementById("forecast-time" + i).innerHTML = time;
-            document.getElementById("weather-icon" + i).src =  weatherIconSRC;
-            document.getElementById("weather" + i).innerHTML = Math.round(data.list[i].main.temp - 273.15) + '°';
-        }
+                if (div.querySelector('img')) div.querySelector('img').remove()
+                div.appendChild(createWeatherIcon(weatherIconSRC))
+                div.innerHTML = 
+                div.innerHTML + `<h3>${Math.round(item.main.temp - 273.15)}&#176;</h3>`
+                hourly.append(div);
     
-        var today = new Date();
+            }
+            if(time.includes("2AM")) isDayPassed = true;
 
-        for(i = 0; i < iconAvg.length; i++){
-            if(i == iconAvg.length - 1) console.log();
-            if(i == 0)document.getElementById("day-label1").innerHTML = "Today";
-            else document.getElementById("day-label" + (i+1)).innerHTML = days[i == iconAvg.length - 1 ? i+today.getDay()-8 : i+today.getDay()-1];
-            document.getElementById("forecast-weather-icon" + (i + 1)).src = iconAvg[i];
-        }
+            if(isDayPassed && counter != 8){
+                avg.push(Math.round(item.main.temp - 273.15))
+                counter++;
+                if(counter == 8){
+                    counter = 0
+                    isDayPassed = false;
+                    main.quickSort(avg, 0, avg.length - 1)
+                    low.push(Math.min(...avg))
+                    high.push(Math.max(...avg))
+                    avg = []
+                }
+    
+            }
+
+        })
+
+
+
+        var today = new Date().getDay();
+        let minus = today;
+        let isPassed = false;
+        let daily =  document.querySelector(".daily-forecast");
+        if (daily.querySelector('div')) daily.querySelectorAll('div').forEach(div => div.remove())
+        iconAvg.forEach((item, index) => {
+            let div = document.createElement('div');
+            div.className = 'five-day-forecast'
+            let day = index == 0 ? 'Today' : days[isPassed ? (today - minus--) : today + index];
+            if(today >= 3 && days[today + index] == 'Sunday')isPassed = true;
+            div.innerHTML = `<h3 class="day-label">${day}</h3>`
+            div.appendChild(createWeatherIcon(item))
+            div.innerHTML = 
+            div.innerHTML + `<h3>${low[index]}&#176;</h3><div class="temp-range"></div><h3>${high[index]}&#176;</h3>`
+            daily.append(div);
+            let lineDiv = document.createElement('div')
+            lineDiv.innerHTML = `<hr style="width: 100%">`
+            div.after(lineDiv)
+            
+        })
+        low = []
+        high = []
+        
+
     },
     getLatLon: async (data) => {
         const { lat, lon} = data[0]
-        console.log("Get Lat Lon:  " + lat, lon)
         await main.fetchFutureWeather(lat, lon)
     },
     getCity: (coordinates) => {
         var xhr = new XMLHttpRequest();
         var lat = coordinates[0];
         var lng = coordinates[1];
-    
-        //pk.6eff11d2dd5c4fbf4f82d3ed41476434
-    
-        xhr.open('GET', "https://us1.locationiq.com/v1/reverse.php?key=pk.6eff11d2dd5c4fbf4f82d3ed41476434&lat=" +
-        lat + "&lon=" + lng + "&format=json", true);
+        var city = ""
+        var index = 0;
+        var callFuncOnce = false
+        xhr.open('GET', `https://us1.locationiq.com/v1/reverse.php?key=${main.locKey}&lat=${lat}&lon=${lng}&format=json`, true);
         xhr.send();
         xhr.onreadystatechange = processRequest;
         xhr.addEventListener("readystatechange", processRequest, false);
-    
         function processRequest(e) {
-            if (xhr.readyState == 4 && xhr.status == 200) {
+            if (xhr.readyState == 4 && xhr.status == 200 && callFuncOnce == false) {
                 var response = JSON.parse(xhr.responseText);
-                var city = response.address.city;
+                city = response.address.city;
                 main.fetchWeather(city);
-                main.fetchFutureWeather(lat, lng);
+                main.fetchFutureWeather(lat, lng)
+                callFuncOnce = true
                 return;
             }
         }
@@ -169,22 +227,15 @@ const main = {
     
           function success(pos) {
             const crd = pos.coords;
-            
             var lat = crd.latitude.toString();
             var lng = crd.longitude.toString();
-    
             var coordinates = [lat, lng];
-    
             main.getCity(coordinates);
-    
             return;
-    
           }
-          
           function error(err) {
             console.warn(`ERROR(${err.code}): ${err.message}`);
           }
-          
           navigator.geolocation.getCurrentPosition(success, error, options);
     },
     dewPointCalc:(temp, humid) => {
@@ -231,11 +282,69 @@ const main = {
     search: async () => {
         var city = document.querySelector(".searchField").value
         const data = await getCityCoordinates(city);
-        main.getLatLon(data)
         await main.fetchWeather(city);
+        main.getLatLon(data)
         document.querySelector(".searchField").value = "";
+
+
+
         return;
-    }
+    },
+    getWeatherState: function () {
+
+    },
+    quickSort: (array, start, end) => {
+        
+
+		//Check if the right side is less or equal than the left side
+		if (start >= end) {
+		
+			return;
+		}
+			
+		
+		//Set all the needed values
+		let pivot = start, left = start + 1, right = end;
+		
+		while (left < right) {
+			//Increment the left while it met the following
+			while (array[left] < array[pivot] && left < right)
+				left++;
+			//Decrement the right while it met the following
+			while (array[right] > array[pivot] && left < right)
+				right--;
+			
+			//Swap the array
+			let temp = array[left];
+			array[left] = array[right];
+			array[right] = temp;
+			if (left < right) {
+				left++;
+				right--;
+			}
+			
+		}
+		
+		if (array[left] > array[pivot]) {
+			//Move the pivot and call recursively
+			let temp = array[pivot];
+			array[pivot] = array[left-1];
+			array[left-1] = temp;
+			main.quickSort(array, start, left-2);
+			main.quickSort(array, left, end);
+			
+		}else {
+			//Move the pivot and call recursively
+			let temp = array[pivot];
+			array[pivot] = array[left];
+			array[left] = temp;
+			main.quickSort(array, start, left-1);
+			main.quickSort(array, left+1, end);
+		
+		}
+
+    },
+
 
 
 }
@@ -250,8 +359,5 @@ document.querySelector(".searchField").addEventListener("keypress", function(eve
     } 
 })
 
-window.onload = () => {
-    main.getCoor
-}
 
 document.addEventListener('DOMContentLoaded', main.init)
--- js/weather-api-service.js
+++ js/weather-api-service.js
@@ -4,7 +4,6 @@ const FORECAST_URL = 'https://api.openweathermap.org/data/2.5/forecast?lat=';
 const API_KEY = '17691d99c39c2957f8d965dc8ffd8343';
 
 
-
 export async function getCurrentWeather(city){
     const forecast = await fetchWeather(city)
     return forecast;
@@ -23,26 +22,23 @@ export async function getCityCoordinates(cityName){
 
 
 async function fetchWeather(city){
+
+
     const url = `${WEATHER_URL}${city}&appid=${API_KEY}`;
     const response = await fetch(url);
     if (!response.ok) {
-          
-        throw new Error(response.statusText);
+        throwError()
     }
     return response.json()
     
 }
 
 async function fetchFutureWeather(lat, lon){
-
     //api.openweathermap.org/data/2.5/forecast?lat=45.4112&lon=-75.6981&appid=17691d99c39c2957f8d965dc8ffd8343
     const url = `${FORECAST_URL}${lat}&lon=${lon}&appid=${API_KEY}`
-    
-    console.log(url)
     const response = await fetch(url)
     if (!response.ok) {
-          
-        throw new Error(response.statusText);
+        throwError()
     }
     return response.json()
     
@@ -50,14 +46,11 @@ async function fetchFutureWeather(lat, lon){
 
 async function fetchCityCoordinates(cityName){
     const url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${API_KEY}`
-    
     const response = await fetch(url)
     if (!response.ok) {
-          
-        throw new Error(response.statusText);
+        throwError()
     }
     return response.json()
-    
 }
 
 export function createWeatherIcon (icon) {
@@ -71,3 +64,6 @@ export function createWeatherIcon (icon) {
   }
 
 
+function throwError() {
+    alert("Please check your input")
+}
